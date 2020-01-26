2a7177aec1c88fa08ba17d01fece9fd6545b0143
Victor Johnson <johnsonsirv@gmail.com>
2020-01-26 23:12:43 +0100

    bug fix, type on filter:completed

--- src/containers/visibleTodoList.jsx
+++ src/containers/visibleTodoList.jsx
@@ -7,7 +7,7 @@ const getVisibleTodos = (todos, filter) => {
     case VisibilityFilters.SHOW_ALL:
       return todos;
     case VisibilityFilters.SHOW_COMPLETED:
-      return todos.filter(t => t.copmleted);
+      return todos.filter(t => t.completed);
     case VisibilityFilters.SHOW_ACTIVE:
       return todos.filter(t => !t.completed);
     default:
